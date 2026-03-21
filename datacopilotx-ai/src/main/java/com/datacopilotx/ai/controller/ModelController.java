552bdafcb9b20c8a05f39765468be6af4a5de6bd
uptown <963842069@qq.com>
2026-03-21 20:29:35 +0800

    fix model delete url

--- datacopilotx-ai/src/main/java/com/datacopilotx/ai/controller/ModelController.java
+++ datacopilotx-ai/src/main/java/com/datacopilotx/ai/controller/ModelController.java
@@ -37,7 +37,7 @@ public class ModelController {
         return WebResult.success(modelConfigService.modify(modifyForm));
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable int id) {
         modelConfigService.del(id);
     }
